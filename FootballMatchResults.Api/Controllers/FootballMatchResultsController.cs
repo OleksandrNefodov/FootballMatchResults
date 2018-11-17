6bf373f9c1ad27626cdee3159c379c1f6c3d5cec
Nefodov Oleksandr <oleksandr.nefodov@aditro.com>
2018-11-17 14:56:27 +0200

    Added FootballMatchResults API project with initial settings.

---
+++ FootballMatchResults.Api/Controllers/FootballMatchResultsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballMatchResults.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballMatchResultsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            //sdasdasdasd
+            return Ok();
+        }    
+    }
+}
